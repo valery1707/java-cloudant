5ce3d022ddaf80aa7e4e6063e28022601cf20ef6
Arun Iyengar <aruni@us.ibm.com>
2015-09-17 22:11:36 -0400

    Initial commit

---
+++ src/main/java/com/cloudant/client/api/DatabaseCache.java
@@ -0,0 +1,417 @@
+/**
+ * 
+ */
+package com.cloudant.client.api;
+
+import static org.lightcouch.internal.CouchDbUtil.assertNotEmpty;
+import static org.lightcouch.internal.CouchDbUtil.close;
+import static org.lightcouch.internal.CouchDbUtil.createPost;
+import static org.lightcouch.internal.CouchDbUtil.getResponse;
+import static org.lightcouch.internal.URIBuilder.buildUri;
+
+import java.net.URI;
+
+import org.apache.http.HttpResponse;
+import org.lightcouch.CouchDatabase;
+import org.lightcouch.DocumentConflictException;
+import org.lightcouch.NoDocumentException;
+import org.lightcouch.Response;
+
+import com.cloudant.client.api.model.Params;
+
+import client.Cache;
+import client.CacheType;
+import client.SameProcessCache;
+import client.Util;
+
+/**
+ * Contains a Database Public API implementation with a cache.
+ * 
+ * @author Arun Iyengar
+ */
+public class DatabaseCache {
+
+    private CouchDatabase db;
+    private CloudantClient client;
+    private Cache<String, Object> cache;
+
+    /**
+     * 
+     * @param database
+     *            : data structure with information about the database
+     *            connection
+     * @param cacheSize
+     *            : maximum number of objects allowed in cache
+     * @param type
+     *            : SAME_PROCESS or OTHER_PROCESS. Currently, only SAME_PROCESS
+     *            is implemented.
+     */
+    public DatabaseCache(Database database, long cacheSize, CacheType type)
+            throws Exception {
+        client = database.getClient();
+        db = database.getDb();
+        switch (type) {
+        case SAME_PROCESS:
+            cache = new SameProcessCache<String, Object>(cacheSize);
+            break;
+        default:
+            throw new Exception(
+                    "Attempt to create a cache type which is not yet supported: "
+                            + type);
+        }
+    }
+
+    /**
+     * put an object into the cache
+     * 
+     * @param <T>
+     *            Object type.
+     * @param id
+     *            The document id.
+     * @param object
+     *            : object to cache
+     * @param lifetime
+     *            : lifetime of the object for the cache in milliseconds
+     */
+    public <T> void cachePut(String id, T object, long lifetime) {
+        cache.put(id, object, Util.getTime() + lifetime);
+    }
+
+    /**
+     * Return value of cached object (or null if not present)
+     * 
+     * @param <T>
+     *            Object type.
+     * @param id
+     *            The document id.
+     * @param classType
+     *            The class of type T.
+     * @return value of object
+     */
+    public <T> T cacheGet(Class<T> classType, String id) {
+        return classType.cast(cache.get(id));
+    }
+
+    /**
+     * Return an object from the cache, if present
+     * 
+     * @param id
+     *            The document id.
+     */
+    public void cacheDelete(String id) {
+        cache.delete(id);
+    }
+
+    /**
+     * Removes all objects from the cache
+     */
+    public void cacheClear() {
+        cache.clear();
+    }
+
+    /**
+     * Returns the cache so that the application can mange it using the Cache
+     * API methods
+     */
+    public Cache<String, Object> getCache() {
+        return cache;
+    }
+
+    /**
+     * Finds an Object of the specified type.
+     * 
+     * @param <T>
+     *            Object type.
+     * @param classType
+     *            The class of type T.
+     * @param id
+     *            The document id.
+     * @param lifetime
+     *            : lifetime of the object for the cache in milliseconds
+     * @return An object of type T.
+     * @throws NoDocumentException
+     *             If the document is not found in the database.
+     */
+    public <T> T find(Class<T> classType, String id, long lifetime) {
+        T value = classType.cast(cache.get(id));
+        if (value != null) {
+            return value;
+        } else {
+            value = db.find(classType, id);
+            cache.put(id, value, Util.getTime() + lifetime);
+            return value;
+        }
+    }
+
+    /**
+     * Finds an Object of the specified type.
+     * 
+     * @param <T>
+     *            Object type.
+     * @param classType
+     *            The class of type T.
+     * @param id
+     *            The document id.
+     * @param params
+     *            Extra parameters to append.
+     * @param lifetime
+     *            : lifetime of the object for the cache in milliseconds
+     * @return An object of type T.
+     * @throws NoDocumentException
+     *             If the document is not found in the database.
+     */
+    public <T> T find(Class<T> classType, String id, long lifetime,
+            Params params) {
+        assertNotEmpty(params, "params");
+        T value = classType.cast(cache.get(id));
+        if (value != null) {
+            return value;
+        } else {
+            value = db.find(classType, id, params.getInternalParams());
+            cache.put(id, value, Util.getTime() + lifetime);
+            return value;
+        }
+    }
+
+    /**
+     * This method finds any document given a URI.
+     * <p>
+     * The URI must be URI-encoded.
+     * 
+     * @param classType
+     *            The class of type T.
+     * @param uri
+     *            The URI as string.
+     * @param lifetime
+     *            : lifetime of the object for the cache in milliseconds
+     * @return An object of type T.
+     */
+    public <T> T findAny(Class<T> classType, String uri, long lifetime) {
+        T value = classType.cast(cache.get(uri));
+        if (value != null) {
+            return value;
+        } else {
+            value = db.findAny(classType, uri);
+            cache.put(uri, value, Util.getTime() + lifetime);
+            return value;
+        }
+    }
+
+    /**
+     * Checks if a document exist in the database.
+     * 
+     * @param id
+     *            The document _id field.
+     * @return true If the document is found, false otherwise.
+     */
+    public boolean contains(String id) {
+        if (cache.get(id) != null)
+            return true;
+        else
+            return db.contains(id);
+    }
+
+    /**
+     * Saves an object in the database, using HTTP <tt>PUT</tt> request.
+     * <p>
+     * If the object doesn't have an <code>_id</code> value, the code will
+     * assign a <code>UUID</code> as the document id.
+     * 
+     * @param object
+     *            The object to save
+     * @param id
+     *            : key to be associated with cached object
+     * @param lifetime
+     *            : lifetime of the object for the cache in milliseconds
+     * @throws DocumentConflictException
+     *             If a conflict is detected during the save.
+     * @return {@link Response}
+     */
+    public <T> com.cloudant.client.api.model.Response save(String id, T object,
+            long lifetime) {
+        Response couchDbResponse = db.save(object);
+        com.cloudant.client.api.model.Response response = new com.cloudant.client.api.model.Response(
+                couchDbResponse);
+        cache.put(id, object, Util.getTime() + lifetime);
+        return response;
+    }
+
+    /**
+     * Saves an object in the database, using HTTP <tt>PUT</tt> request.
+     * <p>
+     * If the object doesn't have an <code>_id</code> value, the code will
+     * assign a <code>UUID</code> as the document id.
+     * 
+     * @param object
+     *            The object to save
+     * @param writeQuorum
+     *            the write Quorum
+     * @param id
+     *            : key to be associated with cached object
+     * @param lifetime
+     *            : lifetime of the object for the cache in milliseconds
+     * @throws DocumentConflictException
+     *             If a conflict is detected during the save.
+     * @return {@link Response}
+     */
+    public <T> com.cloudant.client.api.model.Response save(String id, T object,
+            long lifetime, int writeQuorum) {
+        Response couchDbResponse = client.put(getDBUri(), object, true,
+                writeQuorum, client.getGson());
+        com.cloudant.client.api.model.Response response = new com.cloudant.client.api.model.Response(
+                couchDbResponse);
+        cache.put(id, object, Util.getTime() + lifetime);
+        return response;
+    }
+
+    /**
+     * Saves an object in the database using HTTP <tt>POST</tt> request.
+     * <p>
+     * The database will be responsible for generating the document id.
+     * 
+     * @param object
+     *            The object to save
+     * @param id
+     *            : key to be associated with cached object
+     * @param lifetime
+     *            : lifetime of the object for the cache in milliseconds
+     * @return {@link Response}
+     */
+    public <T> com.cloudant.client.api.model.Response post(String id, T object,
+            long lifetime) {
+        Response couchDbResponse = db.post(object);
+        com.cloudant.client.api.model.Response response = new com.cloudant.client.api.model.Response(
+                couchDbResponse);
+        cache.put(id, object, Util.getTime() + lifetime);
+        return response;
+    }
+
+    /**
+     * Saves an object in the database using HTTP <tt>POST</tt> request with
+     * specificied write quorum
+     * <p>
+     * The database will be responsible for generating the document id.
+     * 
+     * @param object
+     *            The object to save
+     * @param writeQuorum
+     *            the write Quorum
+     * @param id
+     *            : key to be associated with cached object
+     * @param lifetime
+     *            : lifetime of the object for the cache in milliseconds
+     * @return {@link Response}
+     */
+    public <T> com.cloudant.client.api.model.Response post(String id, T object,
+            long lifetime, int writeQuorum) {
+        assertNotEmpty(object, "object");
+        HttpResponse response = null;
+        try {
+            URI uri = buildUri(getDBUri()).query("w", writeQuorum).build();
+            response = client.executeRequest(createPost(uri, client.getGson()
+                    .toJson(object), "application/json"));
+            Response couchDbResponse = getResponse(response, Response.class,
+                    client.getGson());
+            com.cloudant.client.api.model.Response cloudantResponse = new com.cloudant.client.api.model.Response(
+                    couchDbResponse);
+            cache.put(id, object, Util.getTime() + lifetime);
+            return cloudantResponse;
+        } finally {
+            close(response);
+        }
+    }
+
+    /**
+     * Saves a document with <tt>batch=ok</tt> query param.
+     * 
+     * @param object
+     *            The object to save.
+     * @param id
+     *            : key to be associated with cached object
+     * @param lifetime
+     *            : lifetime of the object for the cache in milliseconds
+     */
+    public <T> void batch(String id, T object, long lifetime) {
+        db.batch(object);
+        cache.put(id, object, Util.getTime() + lifetime);
+    }
+
+    /**
+     * Updates an object in the database, the object must have the correct
+     * <code>_id</code> and <code>_rev</code> values.
+     * 
+     * @param object
+     *            The object to update
+     * @param id
+     *            : key to be associated with cached object
+     * @param lifetime
+     *            : lifetime of the object for the cache in milliseconds
+     * @throws DocumentConflictException
+     *             If a conflict is detected during the update.
+     * @return {@link Response}
+     */
+    public <T> com.cloudant.client.api.model.Response update(String id,
+            T object, long lifetime) {
+        Response couchDbResponse = db.update(object);
+        com.cloudant.client.api.model.Response response = new com.cloudant.client.api.model.Response(
+                couchDbResponse);
+        cache.put(id, object, Util.getTime() + lifetime);
+        return response;
+    }
+
+    /**
+     * Updates an object in the database, the object must have the correct
+     * <code>_id</code> and <code>_rev</code> values.
+     * 
+     * @param object
+     *            The object to update
+     * @param writeQuorum
+     *            the write Quorum
+     * @param id
+     *            : key to be associated with cached object
+     * @param lifetime
+     *            : lifetime of the object for the cache in milliseconds
+     * @throws DocumentConflictException
+     *             If a conflict is detected during the update.
+     * @return {@link Response}
+     */
+    public <T> com.cloudant.client.api.model.Response update(String id,
+            T object, long lifetime, int writeQuorum) {
+        Response couchDbResponse = client.put(getDBUri(), object, false,
+                writeQuorum, client.getGson());
+        com.cloudant.client.api.model.Response response = new com.cloudant.client.api.model.Response(
+                couchDbResponse);
+        cache.put(id, object, Util.getTime() + lifetime);
+        return response;
+    }
+
+    /**
+     * Removes a document from the database.
+     * <p>
+     * The object must have the correct <code>_id</code> and <code>_rev</code>
+     * values.
+     * 
+     * @param object
+     *            The document to remove as object.
+     * @param id
+     *            : key to be associated with cached object
+     * @throws NoDocumentException
+     *             If the document is not found in the database.
+     * @return {@link Response}
+     */
+    public <T> com.cloudant.client.api.model.Response remove(String id, T object) {
+        Response couchDbResponse = db.remove(object);
+        cache.delete(id);
+        com.cloudant.client.api.model.Response response = new com.cloudant.client.api.model.Response(
+                couchDbResponse);
+        return response;
+    }
+
+    /**
+     * @return The database URI.
+     */
+    public URI getDBUri() {
+        return db.getDBUri();
+    }
+
+}

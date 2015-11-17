8f571680092aea4e3f2ba04b4d1cd8401ce0b599
Rich Ellis <ricellis@users.noreply.github.com>
2015-11-17 11:07:06 +0000

    Cleaned up interfaces
    Removed redundant public modifiers.
    Removed extraneous `@param` annotations.

--- src/main/java/com/cloudant/client/api/cache/Cache.java
+++ src/main/java/com/cloudant/client/api/cache/Cache.java
@@ -15,21 +15,21 @@ public interface Cache<K, V> {
     /**
      * delete all entries from the cache
      */
-    public void clear();
+    void clear();
 
     /**
      * delete a key-value pair from the cache
      *
      * @param key key corresponding to value
      */
-    public void delete(K key);
+    void delete(K key);
 
     /**
      * delete one or more key-value pairs from the cache
      *
      * @param keys iterable data structure containing the keys to delete
      */
-    public void deleteAll(List<K> keys);
+    void deleteAll(List<K> keys);
 
     /**
      * look up a value in the cache
@@ -38,7 +38,7 @@ public interface Cache<K, V> {
      * @return value corresponding to key, null if key is not in cache or if
      * value is expired
      */
-    public V get(K key);
+    V get(K key);
 
     /**
      * look up one or more values in the cache. Don't return expired values.
@@ -47,7 +47,7 @@ public interface Cache<K, V> {
      * @return map containing key-value pairs corresponding to unexpired data in
      * the cache
      */
-    public Map<K, V> getAll(List<K> keys);
+    Map<K, V> getAll(List<K> keys);
 
     /**
      * look up a CacheEntry in the cache. The CacheEntry may correspond to
@@ -58,14 +58,14 @@ public interface Cache<K, V> {
      * @return value corresponding to key (may be expired), null if key is not
      * in cache
      */
-    public CacheEntry<V> getCacheEntry(K key);
+    CacheEntry<V> getCacheEntry(K key);
 
     /**
      * get cache statistics
      *
      * @return data structure containing statistics
      */
-    public Stats getStatistics();
+    Stats getStatistics();
 
     /**
      * cache a key-value pair
@@ -73,19 +73,18 @@ public interface Cache<K, V> {
      * @param key   key associated with value
      * @param value value associated with key
      */
-    public void put(K key, V value);
+    void put(K key, V value);
 
     /**
      * cache one or more key-value pairs
      *
-     * @param map   map containing key-value pairs to cache
-     * @param value value associated with each key-value pair
+     * @param map map containing key-value pairs to cache
      */
-    public void putAll(Map<K, V> map);
+    void putAll(Map<K, V> map);
 
     /**
-     * Return number of objects in cache
+     * @return number of objects in cache
      */
-    public long size();
+    long size();
 
 }
